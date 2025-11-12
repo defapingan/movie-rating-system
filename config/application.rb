e5c618a120414bd340caf8b4856cb74403e8343b
defapingan <x24251836@student.ncirl.ie>
2025-11-12 10:23:32 +0800

    Update config for production deployment

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module MovieRatingSystem
   class Application < Rails::Application
+    config.hosts.clear
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
