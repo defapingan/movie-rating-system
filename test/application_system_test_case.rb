18fb6029b900a2cb4304e1e8c69e27d6ff700706
defapingan <x24251836@student.ncirl.ie>
2025-12-17 03:15:39 +0800

    Remove Devise and fix CI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,19 +1,5 @@
-# test/application_system_test_case.rb
 require "test_helper"
-require "warden/test/helpers"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
-
-  include Warden::Test::Helpers
-
-  def setup
-    super
-    Warden.test_mode!
-  end
-
-  def teardown
-    super
-    Warden.test_reset!
-  end
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end
