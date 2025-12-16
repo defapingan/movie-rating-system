470071195294508269d49d56061346189fb9af1a
defapingan <x24251836@student.ncirl.ie>
2025-12-17 02:01:59 +0800

    more fix

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,18 @@
 require "test_helper"
+require "warden/test/helpers"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+
+  include Warden::Test::Helpers
+
+  def setup
+    super
+    Warden.test_mode!
+  end
+
+  def teardown
+    super
+    Warden.test_reset!
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,11 +4,6 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  # 运行所有测试
   parallelize(workers: :number_of_processors)
-
-  # 设置所有测试的夹具
   fixtures :all
-
-  # 添加更多辅助方法
 end
