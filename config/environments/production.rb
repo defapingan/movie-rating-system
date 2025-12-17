6c64432a19d65d2ebba2158345c083bc3951ea76
defapingan <x24251836@student.ncirl.ie>
2025-12-17 23:00:34 +0800

    maybe ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,23 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
-  # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+    # Enable DNS rebinding protection and other `Host` header attacks.
+    # config.hosts = [
+    #   "example.com",     # Allow requests from example.com
+    #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+    # ]
+    #
+    # Skip DNS rebinding protection for the default health check endpoint.
+    # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+    config.public_file_server.enabled = true
+    config.assets.compile = true
+
+    config.assets.precompile += %w[application.bootstrap.css]
+
+    config.consider_all_requests_local = true
+
+    config.force_ssl = false
+
+    config.hosts.clear
 end
